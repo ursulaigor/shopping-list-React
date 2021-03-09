47983bc13c57d8466365f97c07af451ab72bd8a9
ursula <ursulaigor@gmail.com>
2021-03-09 00:35:33 -0800

    gh-pages

---
+++ build/precache-manifest.e6a4b518711885842ee0e117b0b6a29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f496a2917ed2e40b70ce879ccce3577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec792e5fc5ebd804f11",
+    "url": "/static/css/main.69be0eb0.chunk.css"
+  },
+  {
+    "revision": "469bfc46514c3ea335eb",
+    "url": "/static/js/2.8ae80734.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ae80734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec792e5fc5ebd804f11",
+    "url": "/static/js/main.8e155467.chunk.js"
+  },
+  {
+    "revision": "5fb11a1c23db92eab440",
+    "url": "/static/js/runtime-main.ebed117f.js"
+  }
+]);
